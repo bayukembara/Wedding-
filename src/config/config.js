ddd7c933288b2d0682c64b6fb3577d38564f515a
Bayu Kembara <bayukembara@gmail.com>
2025-03-26 21:45:48 +0700

    My Wedding

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,17 @@
 const config = {
   data: {
-    title: "Pernikahan Fulan & Fulana",
+    title: "Pernikahan Bayu & Hanisah",
     description:
       "Kami akan menikah dan mengundang Anda untuk turut merayakan momen istimewa ini.",
-    groomName: "Fulan",
-    brideName: "Fulana",
-    parentGroom: "Bapak Groom & Ibu Groom",
-    parentBride: "Bapak Bride & Ibu Bride",
-    date: "2024-12-24",
+    groomName: "Bayu",
+    brideName: "Hanisah",
+    parentGroom: "Bapak Sigid Moeliawan & Ibu Eristya Widyawati",
+    parentBride: "Bapak Mudjiharto Mulyo Widodo & Ibu Yuni Setyo Rahayu",
+    date: "2025-12-27",
     maps_url: "https://goo.gl/maps/abcdef",
     maps_embed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1sid!2sid",
-    time: "16:16 - 17:30 WIB",
+    time: "10:00 - 13:00 WIB",
     location: "Grand Ballroom, Hotel Majesty",
     address: "Jl. Jend. Sudirman No.1, Jakarta",
     ogImage: "/images/og-image.jpg",
@@ -20,25 +20,25 @@ const config = {
       {
         title: "Akad Nikah",
         date: "2024-12-24",
-        startTime: "16:16",
-        endTime: "17:30",
+        startTime: "10:00",
+        endTime: "13:00",
         location: "Grand Ballroom, Hotel Majesty",
         address: "Jl. Jend. Sudirman No.1, Jakarta",
       },
       {
         title: "Resepsi Nikah",
         date: "2024-12-24",
-        startTime: "16:16",
-        endTime: "17:30",
+        startTime: "10:00",
+        endTime: "13:00",
         location: "Grand Ballroom, Hotel Majesty",
         address: "Jl. Jend. Sudirman No.1, Jakarta",
-      }
+      },
     ],
     audio: {
       src: "/audio/fulfilling-humming.mp3",
       title: "Fulfilling Humming",
       autoplay: true,
-      loop: true
+      loop: true,
     },
     banks: [
       {
@@ -50,9 +50,9 @@ const config = {
         bank: "Bank Mandiri",
         accountNumber: "0987654321",
         accountName: "FULANA",
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default config;
