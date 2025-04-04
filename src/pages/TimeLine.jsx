8ad267cfb36c73fe8e347eb1cc3ac4faf4012c1e
Bayu Kembara <bayukembara@gmail.com>
2025-04-04 20:46:00 +0700

    Change timeline font and space between and fix background

--- src/pages/TimeLine.jsx
+++ src/pages/TimeLine.jsx
@@ -10,7 +10,7 @@ function TimeLine() {
       id="timeline"
       className="min-h-screen flex flex-col px-1 py-2 sm:py-5 text-center relative overflow-hidden"
     >
-      <motion.div className="absolute inset-0 w-[100%] h-[100%]">
+      <motion.div className="absolute inset-0 w-full h-full">
         <img
           src={config.data.background_base}
           alt="home"
@@ -21,12 +21,21 @@ function TimeLine() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="space-y-8 relative z-10 w-full max-w-6xl mx-auto"
+        className="space-y-5 relative z-10 w-full max-w-6xl mx-auto"
       >
         <div className="relative">
-          <h2 className="text-2xl font-bold mb-6 z-10 text-[#a84e1d]">
+          <motion.h2
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+            }}
+            className="text-2xl font-bold mb-5 z-10 text-[#a84e1d]"
+          >
             Perjalanan Cinta Kami
-          </h2>
+          </motion.h2>
           {/* Garis tengah utama */}
           <div className="absolute left-1/2 transform -translate-x-1/2 h-[90%] w-0.5 bg-[#e66814]"></div>
 
@@ -36,7 +45,7 @@ function TimeLine() {
               initial={{ opacity: 0, x: index % 2 === 0 ? -30 : 30 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
-              className={`flex items-center justify-center mb-4 ${
+              className={`flex items-center justify-center mb-3 ${
                 index % 2 === 0 ? "flex-row" : "flex-row-reverse"
               }`}
             >
@@ -52,7 +61,7 @@ function TimeLine() {
                     <span className="text-xs text-justify text-balance font-bold text-[#884e0c]">
                       {item.tahun}
                     </span>
-                    <h4 className="text-xs text-justify text-balance  font-semibold text-[#884e0c]">
+                    <h4 className="text-xs text-justify text-balance font-semibold text-[#884e0c]">
                       {item.judul}
                     </h4>
                     <p className="text-xs text-justify text-balance text-[#050505]">
@@ -63,7 +72,7 @@ function TimeLine() {
               </div>
 
               {/* Titik Timeline di Tengah */}
-              <div className="relative w-12 flex items-center justify-center">
+              <div className="relative w-11 flex items-center justify-center">
                 <motion.div
                   whileHover={{ scale: 1.2 }}
                   className="w-2 h-2 bg-blue-500 rounded-full z-10 shadow-sm border border-white"
@@ -71,8 +80,8 @@ function TimeLine() {
 
                 {/* Garis horizontal ke konten */}
                 <div
-                  className={`absolute w-6 h-0.5 bg-blue-400 ${
-                    index % 2 === 0 ? "-left-4" : "-right-4"
+                  className={`absolute w-5 h-0.5 bg-blue-400 ${
+                    index % 2 === 0 ? "-left-3" : "-right-3"
                   }`}
                 ></div>
               </div>
